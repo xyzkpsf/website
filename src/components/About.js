11a20dd361a224eb33d2050f89a6dea6b36e20fa
xyzkpsf <xyzkpsf@gmail.com>
2023-03-04 12:43:49 -0800

    update about, project pages

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,29 @@
 export default function About() {
-    return (
-        <div className="about" id="about">
-            <div className="aboutme">About Me</div>
-            <div className="aboutBox">
-                <div className="avatar">
-                    <img src="image/avatar.png" alt="" />
-                </div>
-                <div className="intro">
-                    <p>
-                        Hello! my name is Xiaoyi, a softwar developer living in
-                        San Francisco. I enjoy using technology to make life
-                        easier. I'm currently working as a Applications
-                        developer at FreeAgent CRM, after graduated from UC
-                        Berkeley EECS in May 2021. The current tech stack I use
-                        are Node.js, React, Redux, GraphQL, Postgres, and so on.
-                        I'm also comfortable with Python and Java.
-                    </p>
-                    <br />
-                    <p>
-                        In addition to wrok, in my free time, I enjoying playing
-                        funny scientific experiments with my sons, listening
-                        folk music, cooking spicy food, and waiting for the new
-                        season of Rick and Morty, "Oooh - Weee!!".
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="about" id="about">
+      <div className="aboutme">About Me</div>
+      <div className="aboutBox">
+        <div className="avatar">
+          <img src="image/avatar.png" alt="" />
         </div>
-    );
+        <div className="intro">
+          <p>Ahoy-hoy!</p>
+          <br />
+          <p>
+            My name is Xiaoyi Zhu and i'm a softwar developer based in San Francisco. I'm currently working at the core platform team at freeAgent CRM, after graduated from UC Berkeley with a degree
+            in EECS in May 2021.
+          </p>
+          <br />
+          <span>
+            In my free time, I enjoying watching tv shows, my favorite ones are Rick and Morty, Peacemaker, The Mandalorian, The Last of Us, and also The Boys, which I made an
+            <a class="apiLink" href="https://www.theboysapi.com" target="_blank">
+              {' '}
+              API{' '}
+            </a>
+            for it.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
